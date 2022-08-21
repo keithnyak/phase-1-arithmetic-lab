172c1f71f6f8b0f1ac73de193af09a4a3423d3fe
keith <nyakinyekeith@gmail.com>
2022-08-21 23:02:03 +0300

    arithmetic lab

---
+++ test/index.js
@@ -0,0 +1,15 @@
+const chai = require('chai')
+const expect = chai.expect;
+
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
+
+const random =math.ceil(math.random()*10);
+
+let num3=24
+let num4=20
+const mod=num3%num4;
+
+const max=math.max(3,7,17,20,4);
+
