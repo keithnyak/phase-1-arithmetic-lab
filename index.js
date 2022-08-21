68d69a3024e31b4590747bb6c03de26387fe2c3e
keith <nyakinyekeith@gmail.com>
2022-08-21 23:04:09 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
-// Write your code here
+const chai = require('chai')
+const expect = chai.expect;
+let num1=31,num2=2;
+function multiply(num1,num2){
+    return num1*num2;
+}
+function random(){
+    return (Number.isInteger(random)*10);
+}
+let num3=24,num4=20;
+function mod(num3,num4){
+    return num3%num4;
+}
+function max(){
+    return Math.max(1,5,7,10,13,17,20);
+}
